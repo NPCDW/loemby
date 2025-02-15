4396ecf37c7f5c82b10553d5e241b6536f451a4b
NPCDW <ME91NGQpq37xpdCT1Z44VD+NPCDW@noreply.cnb.cool>
2025-02-15 23:07:22 +0800

    .

--- README.md
+++ README.md
@@ -1,11 +1,25 @@
-# 相关文档
+# loemby
+
+## 开发
+
+国内网络使用 NPM 镜像源
+```bash
+npm config set registry http://mirrors.cloud.tencent.com/npm/
+```
+
+安装依赖
+```bash
+pnpm i
+```
+
+启动
+```bash
+pnpm run tauri dev
+```
+
+## 相关文档
 - Emby Github [https://github.com/MediaBrowser/Emby/wiki](https://github.com/MediaBrowser/Emby/wiki)
 - Emby Swagger [https://swagger.emby.media/?staticview=true#/](https://swagger.emby.media/?staticview=true#/)
 - Vue [https://vuejs.org/](https://vuejs.org/)
 - Element Plus [https://element-plus.org/zh-CN/](https://element-plus.org/zh-CN/)
 - Tauri V2 [https://v2.tauri.app/](https://v2.tauri.app/)
-
-# NPM 镜像源
-```bash
-npm config set registry http://mirrors.cloud.tencent.com/npm/
-```
--- src/api/embyApi.ts
+++ src/api/embyApi.ts
@@ -1,11 +1,95 @@
 import { fetch } from '@tauri-apps/plugin-http';
+import { generateGuid } from '../util/uuid'
+import { getOsInfo } from '../util/os'
+
+const client = "loemby";
+const version = "0.1.0";
+const user_agent = client + "/" + version;
 
 async function getServerInfo(base_url: string) {
     return fetch(base_url + '/System/Info/Public', {
         method: 'GET',
+        headers: {
+            'User-Agent': user_agent,
+        }
+    });
+}
+
+async function authenticateByName(base_url: string, username: string, password: string) {
+    return fetch(base_url + '/System/Info/Public', {
+        method: 'POST',
+        headers: {
+            'User-Agent': user_agent,
+            'Content-Type': 'application/json',
+            'X-Emby-Authorization': `Emby Client="${client}", Device="${getOsInfo().name}", DeviceId="${generateGuid()}", Version="${version}"`,
+        },
+        body: JSON.stringify({
+            "Username": username,
+            "Pw": password
+        })
+    });
+}
+
+async function logout(base_url: string, token: string) {
+    return fetch(base_url + '/System/Info/Public', {
+        method: 'POST',
+        headers: {
+            'User-Agent': user_agent,
+            'X-Emby-Token': token,
+        }
+    });
+}
+
+async function search(base_url: string, token: string, user_id: string, search_str: string) {
+    return fetch(base_url + `/Users/${user_id}/Items?SearchTerm=${encodeURIComponent(search_str)}&IncludeItemTypes=Movie,Series&Recursive=true`, {
+        method: 'GET',
+        headers: {
+            'User-Agent': user_agent,
+            'X-Emby-Token': token,
+        }
+    });
+}
+
+async function items(base_url: string, token: string, user_id: string, item_id: string) {
+    return fetch(base_url + `/Users/${user_id}/Items/${item_id}`, {
+        method: 'GET',
+        headers: {
+            'User-Agent': user_agent,
+            'X-Emby-Token': token,
+        }
+    });
+}
+
+async function seasons(base_url: string, token: string, item_id: string) {
+    return fetch(base_url + `/Shows/${item_id}/Seasons`, {
+        method: 'GET',
+        headers: {
+            'User-Agent': user_agent,
+            'X-Emby-Token': token,
+        }
+    });
+}
+
+async function episodes(base_url: string, token: string, item_id: string) {
+    return fetch(base_url + `/Shows/${item_id}/Episodes`, {
+        method: 'GET',
+        headers: {
+            'User-Agent': user_agent,
+            'X-Emby-Token': token,
+        }
+    });
+}
+
+async function playbackInfo(base_url: string, token: string, user_id: string, item_id: string) {
+    return fetch(base_url + `/Items/${item_id}/PlaybackInfo?UserId=${user_id}`, {
+        method: 'GET',
+        headers: {
+            'User-Agent': user_agent,
+            'X-Emby-Token': token,
+        }
     });
 }
 
 export default {
-    getServerInfo
+    getServerInfo, authenticateByName, logout, search, items, seasons, episodes, playbackInfo
 }
