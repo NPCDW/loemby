4a8cd2f9995374099f51564e7661660e517112e2
NPCDW <ME91NGQpq37xpdCT1Z44VD+NPCDW@noreply.cnb.cool>
2025-02-27 13:59:44 +0800

    .

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -21,7 +21,6 @@ fn is_development() -> bool {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![get_config, save_config, play_video, http_forward])
         .setup(|app| {
