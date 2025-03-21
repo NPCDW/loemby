2a7e99a0ac95b7e4c9ab94ca4e5ae968e99e990b
NPCDW <chenyongli0520@gmail.com>
2025-03-21 09:42:20 +0800

    .

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -348,7 +348,7 @@ function addEmbyServer() {
     dialogAddEmbyServerVisible.value = true
     invoke.getSysInfo().then(hostname => {
         const client = "loemby";
-        const client_version = "0.6.4";
+        const client_version = "0.6.5";
         const user_agent = client + "/" + client_version;
         useEmbyServer().getMaxOrderBy().then(max_order_by => {
             currentEmbyServer.value = {
